ab55f3fb441d5dd5b35a54760209ce3f52074451
22himanshuvashishtha <149816813+22himanshuvashishtha@users.noreply.github.com>
2025-08-23 09:08:00 +0530

    Create count no of components or separete separate graph count which are not connected directly.cpp

---
+++ count no of components or separete separate graph count which are not connected directly.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+void dfs(int start,vector<int>adj[],vector<int>& visited,vector<int>& ls){
+    visited[start]=1;
+    ls.push_back(start);
+    for(auto it:adj[start]){
+        if(!visited[it]){
+          dfs(it,adj,visited,ls);            
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> adj[n + 1];
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);  
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << i << " -> ";
+        for (auto x : adj[i]) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+
+    vector<int>ls;
+    vector<int> visited(n + 1, 0);
+    int start=1;
+    int count=0;
+    for(int i=1;i<n+1;i++){
+      if(!visited[i]){
+        count++;
+        dfs(start,adj,visited,ls);
+      }    
+    }
+    
+    cout << "DFS traversal: ";
+    for (auto i : ls) cout << i << " ";
+    cout << "\n";
+    cout<<count<<endl;
+    return 0;
+}
